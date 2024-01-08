7d24095e0a86c3b48450bd6bf80df77fa32761a7
Cecil Yen <cecil.yen@nih.gov>
2024-01-08 10:53:21 -0500

    Create Sync_File.cmd

---
+++ Sync_File.cmd
@@ -0,0 +1,3 @@
+@"C:\Windows\System32\net.exe" use q: \\FILESERVER\FOLDER /user:DOMAIN\USER PASSWORD
+@"C:\Program Files\SyncToy 2.1\SyncToyCmd.exe" -R PROFILE
+@"C:\Windows\System32\net.exe" use q: /delete
